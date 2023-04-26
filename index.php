e5a51b73b36aecbb7bfaa73e058ba970be0581a4
utopszkij <tibor.fogler@gmail.com>
2023-04-26 14:12:55 +0200

    v2.1.0

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ importComponent('upgrade');
 $fw = new Fw();
 
 //+ ----------- verzio kezelés start ------------
-$fileVerzio = 'v1.2.0';
+$fileVerzio = 'v2.1.0';
 $upgrade = new \Upgrade();
 $dbverzio  = $upgrade->getDBVersion();
 $lastVerzio = $upgrade->getLastVersion();
